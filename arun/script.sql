ac1634acbc5621b1d159fb61db7ac56910f2ffd4
arunponugoti1 <160697898+arunponugoti1@users.noreply.github.com>
2024-04-21 14:57:13 +0530

    Update script.sql

--- arun/script.sql
+++ arun/script.sql
@@ -1,6 +1,16 @@
-CREATE TABLE $(rcm) (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+USE  to_publish;
+GO
+EXEC sp_dropsubscription
+          @publication = 'pub1',
+          @article     = N'all',
+          @subscriber  = N'34.106.169.79'
+EXEC msdb.dbo.gcloudsql_transrepl_dropsubscriber
+ @subscriber  = N'34.106.169.79'
+EXEC msdb.dbo.gcloudsql_transrepl_droppublication
+  @db = 'to_publish', @publication='pub1'
+
+EXEC msdb.dbo.gcloudsql_transrepl_replicationdboption
+@db='to_publish',
+@value=N'false'
+
+EXEC msdb.dbo.gcloudsql_transrepl_remove_distribution
