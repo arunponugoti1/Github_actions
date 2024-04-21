3e1e282ed38f975080c23228ab1881416485cdfe
arunponugoti1 <160697898+arunponugoti1@users.noreply.github.com>
2024-04-21 07:46:01 +0530

    Update arun.sh

--- arun/arun.sh
+++ arun/arun.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Read name and number from input
-echo "Enter your name: "
-read name
-echo "Enter a number: "
-read number
-
 # Print the name and number
 # echo "Hello, '$(RCM_VALUE)!' You entered the number: $(RCM_VALUE)"
 echo "Hello, '${RCM_VALUE}'! You entered the number: ${RCM_VALUE}"
