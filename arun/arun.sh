06b307409d908287db6cb6fda75b1956d99c11d4
arunponugoti1 <160697898+arunponugoti1@users.noreply.github.com>
2024-04-21 07:10:21 +0530

    Update and rename arun.sql to arun.sh

---
+++ arun/arun.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Read name and number from input
+echo "Enter your name: "
+read name
+echo "Enter a number: "
+read number
+
+# Print the name and number
+echo "Hello, $name! You entered the number: $number"
+
